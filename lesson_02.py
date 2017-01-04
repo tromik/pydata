d15c5d4fe1dcbc2d8e0940a4f7e5768864006da6
tromik <tromik@gmail.com>
2017-01-03 21:05:34 -0500

    Data wrangling lessons

--- lesson_02.py
+++ lesson_02.py
@@ -1,4 +1,17 @@
 import unicodecsv
+from datetime import datetime as dt
+
+def parse_date(date):
+    if date == '':
+        return None
+    else:
+        return dt.strptime(date, '%Y-%m-%d')
+
+def parse_maybe_int(i):
+    if i == '':
+        return None
+    else:
+        return int(i)
 
 def read_csv(filename):
     with open(filename, 'rb') as f:
@@ -8,3 +21,69 @@ def read_csv(filename):
 enrollments = read_csv('enrollments.csv')
 daily_engagement = read_csv('daily_engagement.csv')
 project_submissions = read_csv('project_submissions.csv')
+
+for engagement_record in daily_engagement:
+    engagement_record['account_key'] = engagement_record['acct']
+    del(engagement_record['acct'])
+
+udacity_test_accounts = ()
+for enrollment in enrollments:
+    if enrollment['is_udacity']:
+        udacity_test_accounts.add(enrollment['account_key'])
+
+def remove_udacity_accounts(data):
+    non_udacity_data = []
+    for data_point in data:
+        if data_point['account_key'] not in udacity_test_accounts:
+            non_udacity_data.append(data_point)
+    return non_udacity_data
+
+for enrollment in enrollments:
+    enrollment['cancel_date'] = parse_date(enrollment['cancel_date'])
+    enrollment['days_to_cancel'] = parse_maybe_int(enrollment['days_to_cancel'])
+    enrollment['is_canceled'] = enrollment['is_canceled'] == 'True'
+    enrollment['is_udacity'] = enrollment['is_udacity'] == 'True'
+    enrollment['join_date'] = parse_date(enrollment['join_date'])
+
+for engagement_record in daily_engagement:
+    engagement_record['lessons_completed'] = int(float(engagement_record['lessons_completed']))
+    engagement_record['num_courses_visited'] = int(float(engagement_record['num_courses_visited']))
+    engagement_record['projects_completed'] = int(float(engagement_record['projects_completed']))
+    engagement_record['total_minutes_visited'] = float(engagement_record['total_minutes_visited'])
+    engagement_record['utc_date'] = parse_date(engagement_record['utc_date'])
+
+for submission in project_submissions:
+    submission['completion_date'] = parse_date(submission['completion_date'])
+    submission['creation_date'] = parse_date(submission['creation_date'])
+
+def get_unique_students(data):
+    unqiue_students = set()
+    for data_point in data:
+        unqiue_students.add(data_point['account_key'])
+    return unqiue_students
+
+non_udacity_enrollments = remove_udacity_accounts(enrollments)
+non_udacity_engagements = remove_udacity_accounts(daily_engagement)
+non_udacity_submissions = remove_udacity_accounts(project_submissions)
+
+enrollment_unique_students = get_unique_students(enrollments)
+engagement_unique_students = get_unique_students(daily_engagement)
+submission_unique_students = get_unique_students(project_submissions)
+
+enrollment_num_rows = len(enrollments)
+enrollment_num_unique_students = len(enrollment_unique_students)
+
+engagement_num_rows = len(daily_engagement)
+engagement_num_unique_students = len(engagement_unique_students)
+
+submission_num_rows = len(project_submissions)
+submission_num_unique_students = len(submission_unique_students)
+
+enroll_count = 0
+for enrollment in enrollments:
+    if enrollment['account_key'] not in engagement_unique_students \
+            and enrollment['days_to_cancel'] != 0:
+        print enrollment
+        enroll_count += 1
+
+print enroll_count
